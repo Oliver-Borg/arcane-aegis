7f28e579a2a955a3b7f59d9bb48c4631d5447786
Cameron Worthington <77607587+CamWor@users.noreply.github.com>
2023-10-16 14:51:06 +0200

    Added some sounds
    Skeleton swoosh and hit, undead swoosh and hit, golem roar on spawn. Let player footsteps undone

--- ArcaneAegis/Assets/Scripts/MoveBehaviour.cs
+++ ArcaneAegis/Assets/Scripts/MoveBehaviour.cs
@@ -20,6 +20,9 @@ public class MoveBehaviour : GenericBehaviour
 	private bool jump;                              // Boolean to determine whether or not the player started a jump.
 	private bool isColliding;                       // Boolean to determine if the player has collided with an obstacle.
 
+
+
+
 	// Start is always called after any Awake functions.
 	void Start()
 	{
@@ -131,6 +134,7 @@ public class MoveBehaviour : GenericBehaviour
 		}
 
 		behaviourManager.GetAnim.SetFloat(speedFloat, speed, speedDampTime, Time.deltaTime);
+
 	}
 
 	// Remove vertical rigidbody velocity.
